87a8b9a7db1089f31cbb0b4cf295821b1021681d
Toolcats3276 <toolcatsrobotics@gmail.com>
2026-01-24 14:13:30 -0600

    Base

---
+++ src/main/java/frc/robot/subsystems/SwerveSS.java
@@ -0,0 +1,238 @@
+package frc.robot.subsystems;
+
+import frc.robot.SwerveModule;
+import frc.robot.vision.LimelightAssistant;
+import frc.robot.Constants.Swerve;
+import frc.robot.Robot;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+
+import static edu.wpi.first.units.Units.Degrees;
+
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.VecBuilder;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveSS extends SubsystemBase {
+    public SwerveDriveOdometry swerveOdometry;
+    public SwerveModule[] mSwerveMods;
+    public static Pigeon2 gyro;
+    
+    public static PIDController LLTranslationFR;
+    public static PIDController LLStrafeFR;
+    public static PIDController LLTranslationFL;
+    public static PIDController LLStrafeFL;
+
+    public static LimelightAssistant LLAssistantFR;
+    public static LimelightAssistant LLAssistantFL;
+
+
+    public static SwerveDrivePoseEstimator m_poseEstimator;
+
+    public Field2d LLPose;
+    public Field2d BotPose;
+    public boolean scoreLeft;
+
+
+    @SuppressWarnings("unused")
+    private NeutralModeValue driveNeutralMode;
+    
+    
+        public SwerveSS() {
+    
+            driveNeutralMode = NeutralModeValue.Brake;
+    
+            gyro = new Pigeon2(Swerve.pigeonID);
+            gyro.getConfigurator().apply(new Pigeon2Configuration());
+            gyro.setYaw(0);
+    
+            mSwerveMods = new SwerveModule[] {
+                new SwerveModule(0, Swerve.Mod0.constants),
+                new SwerveModule(1, Swerve.Mod1.constants),
+                new SwerveModule(2, Swerve.Mod2.constants),
+                new SwerveModule(3, Swerve.Mod3.constants)
+            };
+    
+            swerveOdometry = new SwerveDriveOdometry(Swerve.swerveKinematics, getGyroYaw(), getModulePositions());
+
+
+            LLTranslationFR = new PIDController(.09, 0, 0.02);
+            LLStrafeFR = new PIDController(0.01, 0, 0.0005);
+            LLTranslationFL = new PIDController(.037, 0, 0.03);
+            LLStrafeFL = new PIDController(0.007, 0, 0.0009);
+
+            LLAssistantFR = new LimelightAssistant("limelight-fr", VecBuilder.fill(0,0,0), false);
+            LLAssistantFL = new LimelightAssistant("limelight-fl", VecBuilder.fill(0,0,0), false);
+            
+    
+                
+        }
+    
+        public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+            SwerveModuleState[] swerveModuleStates =
+                Swerve.swerveKinematics.toSwerveModuleStates(
+                    fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                            translation.getX(), 
+                            translation.getY(), 
+                            rotation, 
+                            getHeading()
+                        )
+                        : new ChassisSpeeds(
+                            translation.getX(), 
+                            translation.getY(), 
+                            rotation)
+                        );
+            SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Swerve.maxSpeed);
+    
+            for(SwerveModule mod : mSwerveMods){
+                mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+            }
+        }  
+        
+        
+        public void driveRobotRelative(ChassisSpeeds robotRelativeSpeeds) {
+            ChassisSpeeds targetSpeeds = ChassisSpeeds.discretize(robotRelativeSpeeds, 0.02);
+    
+            SwerveModuleState[] targetState = Swerve.swerveKinematics.toSwerveModuleStates(targetSpeeds);
+            // SwerveDriveKinematics.desaturateWheelSpeeds(targetState, Swerve.maxSpeed);
+    
+            for(SwerveModule mod : mSwerveMods){
+                mod.setDesiredState(targetState[mod.moduleNumber], false);
+            }
+    
+          }
+        // public void driveRobotRelative(ChassisSpeeds robotRelativeSpeeds) {
+        //     ChassisSpeeds targetSpeeds = ChassisSpeeds.discretize(robotRelativeSpeeds, 0.02);
+        
+        //     SwerveModuleState[] targetStates = Swerve.swerveKinematics.toSwerveModuleStates(targetSpeeds);
+        //     setModuleStates(targetStates);
+        //   }
+    
+        /* Used by SwerveControllerCommand in Auto */
+        public void setModuleStates(SwerveModuleState[] desiredStates) {
+            SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Swerve.maxSpeed);
+            
+            for(SwerveModule mod : mSwerveMods){
+                mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+            }
+        }
+    
+        public SwerveModuleState[] getModuleStates(){
+            SwerveModuleState[] states = new SwerveModuleState[4];
+            for(SwerveModule mod : mSwerveMods){
+                states[mod.moduleNumber] = mod.getState();
+            }
+            return states;
+        }
+    
+        public SwerveModulePosition[] getModulePositions(){
+            SwerveModulePosition[] positions = new SwerveModulePosition[4];
+            for(SwerveModule mod : mSwerveMods){
+                positions[mod.moduleNumber] = mod.getPosition();
+            }
+            return positions;
+        }
+    
+        public ChassisSpeeds getRobotSpeed(){
+            return Swerve.swerveKinematics.toChassisSpeeds(getModuleStates());
+        }
+    
+        public Pose2d getPose() {
+            return swerveOdometry.getPoseMeters();
+        }
+    
+        public Pose2d getPoseEstimate(){
+            return m_poseEstimator.getEstimatedPosition();
+        }
+    
+        public void setPose(Pose2d pose) {
+            swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), pose);
+        }
+    
+        public void resetPoseEstimate(Pose2d pose){
+            m_poseEstimator.resetPosition(getGyroYaw(), getModulePositions(), pose);
+        }
+    
+        public Rotation2d getHeading(){
+            return getPose().getRotation();
+        }
+    
+        public void setHeading(Rotation2d heading){
+            swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), heading));
+        }
+    
+        public void zeroHeading(){
+            swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), new Rotation2d()));
+        }
+    
+        public Rotation2d getGyroYaw() {
+            return Rotation2d.fromDegrees(gyro.getYaw().getValue().in(Degrees));
+        }
+    
+        public void resetModulesToAbsolute(){
+            for(SwerveModule mod : mSwerveMods){
+                mod.resetToAbsolute();
+            }
+        }
+    
+    
+        public void setNeutralMode(NeutralModeValue driveNeutralMode){
+            this.driveNeutralMode = driveNeutralMode;
+            TalonFXConfiguration swerveDriveFXConfig = Robot.ctreConfigs.swerveDriveFXConfig;
+            swerveDriveFXConfig.MotorOutput.NeutralMode = driveNeutralMode;
+
+            mSwerveMods[0].mDriveMotor.getConfigurator().apply(swerveDriveFXConfig);
+            mSwerveMods[1].mDriveMotor.getConfigurator().apply(swerveDriveFXConfig);
+            mSwerveMods[2].mDriveMotor.getConfigurator().apply(swerveDriveFXConfig);
+            mSwerveMods[3].mDriveMotor.getConfigurator().apply(swerveDriveFXConfig);
+        }
+
+
+    @SuppressWarnings("removal")
+    @Override
+    public void periodic(){
+        swerveOdometry.update(getGyroYaw(), getModulePositions());
+        
+
+
+        for(SwerveModule mod : mSwerveMods){
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " CANcoder", mod.getCANcoder().getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", mod.getPosition().angle.getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);    
+        }
+
+        
+        SmartDashboard.putNumber("Rotation Output", RotationCalculateFL());
+        SmartDashboard.putNumber("Rotation TX", FrontTX());
+
+
+
+    }
+
+
+    public double FrontTX(){
+        return LLAssistantFL.getTX()-LLAssistantFR.getTX();
+    }
+
+    public double RotationCalculateFL(){
+        return LLTranslationFR.calculate(FrontTX(), 0);
+    }
+}
---
+++ src/main/java/frc/robot/vision/LimelightAssistant.java
@@ -0,0 +1,371 @@
+package frc.robot.vision;
+
+import static edu.wpi.first.units.Units.Degree;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.Radians;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.SwerveSS;
+import frc.robot.vision.LimelightHelpers.PoseEstimate;
+
+public class LimelightAssistant {
+
+    private String limelightName;
+    private Matrix<N3,N1> visionMeasurementStdDevs;
+    private boolean useMegaTag1;
+
+    private boolean doRejectUpdate;
+
+    LimelightHelpers.PoseEstimate megaTag1PoseEstimator;
+    private Pose2d megaTag1Pose2d;
+    private double megaTag1Timestamp;
+
+    LimelightHelpers.PoseEstimate megaTag2PoseEstimator;
+    private Pose2d megaTag2Pose2d;
+    private double megaTag2Timestamp;
+
+
+    private final PIDController rotationPidController;
+    private final PIDController translationPidController;
+
+
+    
+    /**
+   * Constructs a LimelightUpdater to quickly add multiple limelights to the robot for tag allignment and pose estimation.
+   *
+   * @param limelightName The name of the limelight as set in the WebUI.
+   * @param visionMeasurementStdDevs Standard deviations of the vision pose measurement (x position
+   *     in meters, y position in meters, and heading in radians). Increase these numbers to trust
+   *     the vision pose measurement less (can be changed later).
+   * @param useMegaTag1 This will give mega tag 1 estimates if true and mega tag 2 estimates if false (can be changed later).
+   */
+
+    public LimelightAssistant(String limelightName,Matrix<N3,N1> visionMeasurementStdDevs, boolean useMegaTag1){
+        
+        this.limelightName = limelightName;
+        this.visionMeasurementStdDevs = visionMeasurementStdDevs;
+        this.useMegaTag1 = useMegaTag1;
+
+        doRejectUpdate = false;
+
+
+        megaTag1PoseEstimator = new PoseEstimate();
+        megaTag1PoseEstimator = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+        megaTag1Pose2d = new Pose2d();
+
+        megaTag2PoseEstimator = new PoseEstimate();
+        megaTag2PoseEstimator = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+        megaTag2Pose2d = new Pose2d();
+
+        rotationPidController = new PIDController(0.006, 0, 0);
+        translationPidController = new PIDController(0.001, 0, 0);
+
+    }
+
+    /**
+     * @return Horizontal Offset From Crosshair To Target.
+     */
+    public double getTX(){
+        return LimelightHelpers.getTX(limelightName);
+    }
+
+    /**
+     * @return Vertical Offset From Crosshair To Target.
+     */
+    public double getTY(){
+        return LimelightHelpers.getTY(limelightName);
+    }
+
+    /**
+     * @return Target Area (0% of image to 100% of image).
+     */
+    public double getTA(){
+        return LimelightHelpers.getTA(limelightName);
+    }
+
+    /**
+     * @return True if valid target exists. False if no valid targets exist.
+     */
+    public boolean getTV(){
+        return LimelightHelpers.getTV(limelightName);
+    }
+
+    /**
+     * @return The Fiducial tag ID that is being targeted.
+     */
+    public double getFiducialID(){
+        return LimelightHelpers.getFiducialID(limelightName);
+    }
+
+
+    /**
+     * Method to calculate the rotational output for drivetrain aiming using TX as a measurment.
+     * Needs PID Constants tuned in Limelight Constants.
+     * @param setPoint The desired point to aim at.
+     * @return The output for drivetrain aiming
+     */
+    public double calculateRotationOutput(double setPoint){
+        double output;
+
+        if(getTV()){
+        output = rotationPidController.calculate(getTX(), setPoint);
+        }
+        else{
+            output = 0;
+        }
+
+        return output;
+    }
+
+    /**
+     * Method to calculate the translational output for drivetrain ranging using TY as a measurment.
+     * Needs PID Constants tuned in Limelight Constants.
+     * @param setPoint The desired point to range to.
+     * @return The output for drivetrain ranging
+     */
+    public double calculateTranslationOutput_TY(double setPoint){
+        double output;
+
+        if(getTV()){
+        output = translationPidController.calculate(getTY(), setPoint);
+        }
+        else{
+            output = 0;
+        }
+
+        return output;
+    }
+
+    /**
+     * Method to calculate the translational output for drivetrain ranging using TX as a measurment.
+     * Needs PID Constants tuned in Limelight Constants.
+     * @param setPoint The desired point to range to.
+     * @return The output for drivetrain ranging
+     */
+    public double calculateTranslationOutput_TX(double setPoint){
+        double output;
+
+        if(getTV()){
+        output = translationPidController.calculate(getTX(), setPoint);
+        }
+        else{
+            output = 0;
+        }
+
+        return output;
+    }
+
+    /**
+     * Method to calculate the translational output for drivetrain ranging using TA as a measurment.
+     * Needs PID Constants tuned in Limelight Constants.
+     * @param setPoint The desired point to range to.
+     * @return The output for drivetrain ranging
+     */
+    public double calculateTranslationOutput_TA(double setPoint){
+        double output;
+
+        if(getTV()){
+        output = translationPidController.calculate(100 - getTA(), setPoint);
+        }
+        else{
+            output = 0;
+        }
+
+        return output;
+    }
+
+
+    /**
+     * @param visionMeasurementStdDevs Standard deviations of the vision pose measurement (x position
+     * in meters, y position in meters, and heading in radians). Increase these numbers to trust
+     * the vision pose measurement less (can be changed later).
+     */
+    public void setVisionMeasurementStdDevs(Matrix<N3,N1> visionMeasurementStdDevs){
+        this.visionMeasurementStdDevs = visionMeasurementStdDevs;
+    }
+
+    /** 
+     * @return The standard deviations of the vision pose measurments (x position
+     * in meters, y position in meters, and heading in radians). Increase these numbers to trust
+     * the vision pose measurement less (can be changed later).
+    */
+    public Matrix<N3,N1> getVisionMeasurementStdDevs(){
+        return visionMeasurementStdDevs;
+    }
+
+    /**
+     * @param useMegaTag1 This will change weather or not to give mega tag 1 estimates if true and mega tag 2 estimates if false.
+     */
+    public void setMegaTagMode(boolean useMegaTag1){
+        this.useMegaTag1 = useMegaTag1;
+    }
+
+    /**
+     * @return This will give mega tag 1 estimates if true and mega tag 2 estimates if false.
+     */
+    public boolean getMegaTagMode(){
+        return useMegaTag1;
+    }
+
+    /**
+     * @return weather or not limelight pose should be updated
+     */
+    // public boolean rejectUpdate(){
+
+    //     if(useMegaTag1 == true)
+    //     {
+    //         if(megaTag1PoseEstimator != null){
+
+    //         LimelightHelpers.PoseEstimate megaTag1PoseEstimator = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+
+    //           if(megaTag1PoseEstimator.tagCount == 1 & megaTag1PoseEstimator.rawFiducials.length == 1)
+    //           {
+    //             if(megaTag1PoseEstimator.rawFiducials[0].ambiguity > .7)
+    //             {
+    //               doRejectUpdate = true;
+    //             }
+    //             if(megaTag1PoseEstimator.rawFiducials[0].distToCamera > 3)
+    //             {
+    //               doRejectUpdate = true;
+    //             }
+    //           }
+    //           if(megaTag1PoseEstimator.tagCount == 0)
+    //           {
+    //             doRejectUpdate = true;
+    //           }
+    //         }
+    //     }
+
+        // else if (useMegaTag1 == false & megaTag2PoseEstimator != null)
+        // {
+            
+        // LimelightHelpers.SetRobotOrientation(limelightName, SwerveSS.m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+        // LimelightHelpers.PoseEstimate megaTag2PoseEstimator = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+
+        //   if(Math.abs(SwerveSS.gyro.getYaw().in(Degrees)) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+        //   {
+        //     doRejectUpdate = true;
+        //   }
+        //   if(megaTag2PoseEstimator.tagCount == 0)
+        //   {
+        //     doRejectUpdate = true;
+        //   }
+        // }
+        
+        // else{
+        //     doRejectUpdate = false;
+        // }
+    
+        // return doRejectUpdate;
+    // }
+
+    /**
+     * Updates mega tag 1 and 2 variables with vision measurments. 
+     * This should be called every loop.
+     */
+    public void updatePoseEstimates(){
+        megaTag1PoseEstimator = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+        megaTag2PoseEstimator = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+        // updatePoseEstimateVariables();
+
+        
+        publishSmartDashboard();
+    }
+
+    /**
+     * Updates mega tag 1 and 2 Pose2d and Timestamp variables
+     */
+    // private void updatePoseEstimateVariables(){
+    //     if(!rejectUpdate()){
+    //         if(useMegaTag1 & megaTag1PoseEstimator != null){
+    //             megaTag1Pose2d = megaTag1PoseEstimator.pose;
+    //             megaTag1Timestamp = megaTag1PoseEstimator.timestampSeconds;
+    //         }
+    //         else if(!useMegaTag1 & megaTag2PoseEstimator != null){
+    //             megaTag2Pose2d = megaTag2PoseEstimator.pose;
+    //             megaTag2Timestamp = megaTag2PoseEstimator.timestampSeconds;
+    //         }
+    //     }
+    // }
+
+    /**
+     * @return The vision pose measurments taken from the limelight.
+     */
+    public Pose2d getPoseEstimate(){
+        if(useMegaTag1 == true){
+            return megaTag1Pose2d;
+        }
+        else{
+            return megaTag2Pose2d;
+        }
+    }
+
+    /**
+     * @return The time stamp taken from the limelight.
+     */
+    public double getTimestamp(){
+        if(useMegaTag1 == true){
+            return megaTag1Timestamp;
+        }
+        else{
+            return megaTag2Timestamp;
+        }
+    }
+
+
+    // public Matrix<N3,N1> proportionalStdDev(){
+    //     Translation2d tagTranslation2d = LimelightHelpers.getTargetPose3d_RobotSpace(limelightName).getTranslation().toTranslation2d();
+    //     double tagDistance = tagTranslation2d.getNorm();
+    //     Matrix<N3,N1> proportionalStdDev = VecBuilder.fill(tagDistance * 1, tagDistance * 1, 99999999);
+    //     return proportionalStdDev;
+    // }
+    public double getTagDistance(){
+        Translation3d tagTranslation3d = LimelightHelpers.getTargetPose3d_RobotSpace(limelightName).getTranslation();
+        double tagDistance = tagTranslation3d.getNorm();
+        Matrix<N3,N1> proportionalStdDev = VecBuilder.fill(tagDistance * 1, tagDistance * 1, 99999999);
+        return tagDistance;
+    }
+
+
+    public void publishSmartDashboard(){
+        SmartDashboard.putNumber(limelightName + " TX", getTX());
+        SmartDashboard.putNumber(limelightName + " TY", getTY());
+        SmartDashboard.putNumber(limelightName + " TA", getTA());
+        SmartDashboard.putBoolean(limelightName + " TV", getTV());
+        SmartDashboard.putNumber(limelightName + " Tag ID", getFiducialID());
+        SmartDashboard.putNumber(limelightName + "Tag Distance", getTagDistance());
+    }
+
+
+    /**
+     * Method to force Limelight LEDs on
+     */
+    public void forceOn(){
+        LimelightHelpers.setLEDMode_ForceOn(limelightName);
+    }
+
+    /**
+     * Method to force Limelight LEDs off
+     */
+    public void forceOff(){
+        LimelightHelpers.setLEDMode_ForceOff(limelightName);
+    }
+
+    /**
+     * Method to force Limelight LEDs to blink
+     */
+    public void forceBlink(){
+        LimelightHelpers.setLEDMode_ForceBlink(limelightName);
+    }
+
+    
+
+}
